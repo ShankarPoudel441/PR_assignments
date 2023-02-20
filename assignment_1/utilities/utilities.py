89b2cd890ec7d267f8b24be7de1141aafac12aff
Shankar <shankarpoudel441@gmail.com>
2023-02-20 11:25:51 -0800

    Added the data generation

--- assignment_1/utilities/utilities.py
+++ assignment_1/utilities/utilities.py
@@ -1,1 +1,16 @@
 import numpy as np
+
+def generate_nd_normal_uncorrleated(number, mus, sigmas):
+    data=np.array([np.random.normal(m,s,number) for m,s in zip(mus,sigmas)])
+    return data.T
+
+def generate_data_uncorrleated(numbers,mus,sigmas):
+    data=[]
+    for idx,mu in enumerate(mus):
+        a=generate_nd_normal_uncorrleated(numbers[idx],mu,sigmas[idx])
+        b=np.hstack((a,[[idx]]*len(a)))
+        try:
+            data=np.concatenate((data,b))
+        except:
+            data=b
+    return data
